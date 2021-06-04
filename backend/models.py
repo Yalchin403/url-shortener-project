50cad9fabeea67edcd441c36b2d32faf20f9bee0
mrRootLog <yalchin.mammadli.std@bhos.edu.az>
2021-06-04 21:56:19 +0400

    original length type changed from charfield to textfield, copy button now has 2px margin

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class ShortenedUrl(models.Model):
-    originalUrl = models.CharField(max_length=200)
+    originalUrl = models.TextField()
     shortenedUrl = models.CharField(max_length=6, blank = True, null = True)
     createdTime = models.DateTimeField(auto_now_add=True) # won't be seen in admin as it's not editable
     updated_at = models.DateTimeField(auto_now=True)
