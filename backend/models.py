6c5e9328eabea22d499336d85c5366a9c5631b5c
Yalchin Mammadli <yalchinmammadli@outlook.com>
2021-07-25 23:56:12 +0400

    title model field type changed to textfield, homepage design polished, base.html polished

--- backend/models.py
+++ backend/models.py
@@ -7,7 +7,7 @@ class ShortenedUrl(models.Model):
     shortenedUrl = models.CharField(max_length=6, blank = True, null = True)
     createdTime = models.DateTimeField(auto_now_add=True) # won't be seen in admin as it's not editable
     updated_at = models.DateTimeField(auto_now=True)
-    title = models.CharField(max_length=50, null=True, blank=True)
+    title = models.TextField(null=True, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     
     def __str__(self):
