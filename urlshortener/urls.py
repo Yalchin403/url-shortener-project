a3d3a2ff45080b39b3c81c56c0434685378d5439
Yalchin Mammadli <yalchin.mammadli.std@bhos.edu.az>
2021-10-18 02:44:21 +0400

    admin sidebar disable

--- urlshortener/urls.py
+++ urlshortener/urls.py
@@ -17,6 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib import admin
+
+
+admin.autodiscover()
+admin.site.enable_nav_sidebar = False
 
 
 urlpatterns = [
