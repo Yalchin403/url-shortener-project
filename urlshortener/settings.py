7b58f68efd46c2d6f0bf632d1b8e924f44f60ee2
Yalchin Mammadli <yalchin.mammadli.std@bhos.edu.az>
2021-10-18 02:35:55 +0400

    ckeditor admin error

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -129,6 +129,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
